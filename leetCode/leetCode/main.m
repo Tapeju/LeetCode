e809330b8b967fe3bdc3a468008b280530dc1bfc
PJTang <673795524@qq.com>
2021-07-26 15:07:19 +0800

    feat:创建项目

---
+++ leetCode/leetCode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  leetCode
+//
+//  Created by Ton on 2021/7/26.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
